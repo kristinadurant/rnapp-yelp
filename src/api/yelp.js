1fcd5c13bf38874ecb52560a1c0c7da3c1a34fe1
kristinadurant <kristinadurant.p@gmail.com>
2021-01-21 14:02:04 -0500

    api configuration

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
     baseURL: 'https://api.yelp.com/v3/businesses',
     headers: {
-        Authorization: 'Bearer WGQw7WgbSS3H-OjUjfs0sE736Xo6hf_hExM4pVRRP8zaX3InC0Csma-051aSwGJXeZZ1lKbeG7GIxLXTJasHzoFgYlNdLCZci_wEFL0uen5OW_yZTWGIhpV29j8IYHYx'
+        Authorization: `Bearer ${YELP_API_KEY}`
     }
 });
